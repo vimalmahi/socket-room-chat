86f1981ca036597aadaa8446ea0df72a00fcc769
vimal <vimal.maheedharan@cabotsolutions.com>
2016-08-28 22:30:20 +0530

    Added logic to handle multiple users

--- public/js/lib.js
+++ public/js/lib.js
@@ -1,4 +1,3 @@
-var socket = io.connect('http://localhost:3000/');
 
 var loggedInUser;
 var roomName;
@@ -7,12 +6,94 @@ var counter = 0;
 
 $(function(){
 	var timeInterval;
+	$("#chatSection").hide();
+	var socket = io.connect('http://localhost:3000/');
+
+	$('#loginSelectBtn').click(function(event){
+		loggedInUser = $("#userSelect").find(":selected").text();
+		socket.emit('userJoin', {
+			userName:loggedInUser,
+			roomName:roomName,
+			emitEvent:true
+		});
+	});
+
+	$("#chatSelectBtn").click(function(event){
+		roomName = $(".roomSelect:visible").find(":selected").text();
+		socket.emit('userJoin', {
+			userName:loggedInUser,
+			roomName:roomName,
+			emitEvent:true
+		});
+		
+
+		if($('.chat_window#'+roomName).length == 0) {
+			$chat_window = $($('#chat_wrapper .chat_window').clone().prop('id', roomName));
+			$('#chat_area').append($chat_window);	
+			$('.chat_window#'+roomName).show('slow');
+			$('.chat_window#'+roomName).draggable();
+			$('.chat_window#'+roomName+' .title').eq(0).html(roomName);
+
+			$('.chat_window#'+roomName+' .send_message').click(function(){
+				socket.emit('sendChatMsg', {
+					roomName:roomName,
+					from:loggedInUser,
+					msg: $('.chat_window#' +roomName+ ' .message_input').eq(0).val()
+				});	
+			});
+
+			$('.chat_window#'+roomName+' .message_input').keyup(function(e){
+			    if(e.keyCode == 13)
+			    {
+			        $('.chat_window#'+roomName+" .send_message").trigger("click");
+			    }
+			});
+		}
+		
+
+
+
+		var users = $("#userChatSelect").find(":selected");
+		var chatWith = users.map(function() { 
+                                       return this.value; 
+                                   }).get().join(',');
+
+
+		var usersArray = []; 
+		$('#userChatSelect :selected').each(function(i, selected){ 
+	  		usersArray[i] = $(selected).text(); 
+		});
+
+		
+		socket.emit('addUsersToChat', {
+			from: [loggedInUser],
+			to: usersArray,
+			roomName: roomName
+		});
+	});
+
+	$('#socketSelectBtn').click(function(event) {
+		$("#loginSection").hide('slow');
+		loggedInUser = $("#userSelect").find(":selected").text();
+		$("#loggedInMsg").html('<b>'+ loggedInUser +"</b> is listening for chats now.");
+		if (typeof timeInterval === 'undefined') {
+			timeInterval = setInterval(function() {
+			    socket.emit('userJoin', {
+					userName:loggedInUser,
+					emitEvent: false
+				}); 
+			}, 5000);
+		}
+	})
+
 	socket.on('userJoinNotify', function(data) {
 	
-		$("#loggedInMsg").html('<b>'+ data.userName +"</b> logged in to <b>" + data.roomName + "</b>");
+		$("#loggedInMsg").html('<b>'+ data.userName +"</b> is logged in.");
 		$("#loginSection").hide('slow');
 
 		$("#chatSection").show();
+		$(".roomSelect option:contains("+roomName+")").prop("selected", true);
+
 		$("#userChatSelect option[value='"+data.userName+"']").hide();
 
 		if (typeof timeInterval === 'undefined') {
@@ -29,7 +110,7 @@ $(function(){
 	socket.on('sendChatMsgNotify', function(time, data) {
 		
 		if($('.chat_window#'+data.roomName).length == 0) {
-			$chat_window = $($('.chat_window').clone().prop('id', data.roomName));
+			$chat_window = $($('#chat_wrapper .chat_window').clone().prop('id', data.roomName));
 			$('#chat_area').append($chat_window);	
 			$('.chat_window#'+data.roomName).show('slow');
 			$('.chat_window#'+data.roomName+' .title').eq(0).html(data.roomName);
@@ -39,7 +120,7 @@ $(function(){
 				socket.emit('sendChatMsg', {
 					roomName:data.roomName,
 					from:loggedInUser,
-					msg: $(".message_input").eq(0).val()
+					msg: $('.chat_window#' +data.roomName+ '.message_input').eq(0).val()
 				});	
 			});
 
@@ -81,85 +162,14 @@ $(function(){
             room: room
         });
         message.draw();
-       // return message;
-        return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+        return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 0);
     };
 
-	$("#chatSection").hide();
-	$('#loginSelectBtn').click(function(event){
-		loggedInUser = $("#userSelect").find(":selected").text();
-		roomName = $("#roomSelect").find(":selected").text();
-		socket.emit('userJoin', {
-			userName:loggedInUser,
-			roomName:roomName,
-			emitEvent:true
-		});
-	});
 
-	$('#socketSelectBtn').click(function(event) {
-		$("#loginSection").hide('slow');
-		loggedInUser = $("#userSelect").find(":selected").text();
-		$("#loggedInMsg").html('<b>'+ loggedInUser +"</b> is listening for chats now.");
-		if (typeof timeInterval === 'undefined') {
-			timeInterval = setInterval(function() {
-			    socket.emit('userJoin', {
-					userName:loggedInUser,
-					emitEvent: false
-				}); 
-			}, 5000);
-		}
-	})
+	
 
 
-	$("#chatSelectBtn").click(function(event){
-		$("#chatSection").hide('slow');
-		
-
-
-		if($('.chat_window#'+roomName).length == 0) {
-			$chat_window = $($('.chat_window').clone().prop('id', roomName));
-			$('#chat_area').append($chat_window);	
-			$('.chat_window#'+roomName).show('slow');
-			$('.chat_window#'+roomName).draggable();
-			$('.chat_window#'+roomName+' .title').eq(0).html(roomName);
-
-			$('.chat_window#'+roomName+" .send_message").click(function(){
-				socket.emit('sendChatMsg', {
-					roomName:roomName,
-					from:loggedInUser,
-					msg: $(".message_input").eq(0).val()
-				});	
-			});
-
-			$('.chat_window#'+roomName+' .message_input').keyup(function(e){
-			    if(e.keyCode == 13)
-			    {
-			        $('.chat_window#'+roomName+" .send_message").trigger("click");
-			    }
-			});
-		}
-		
-
-
-
-		var users = $("#userChatSelect").find(":selected");
-		var chatWith = users.map(function() { 
-                                       return this.value; 
-                                   }).get().join(',');
-
-
-		var usersArray = []; 
-		$('#userChatSelect :selected').each(function(i, selected){ 
-		  usersArray[i] = $(selected).text(); 
-		});
-
-		
-		socket.emit('addUsersToChat', {
-			from: [loggedInUser],
-			to: usersArray,
-			roomName: roomName
-		});
-	});
+	
 
 
 });
